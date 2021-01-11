bff613227ee61e821457b33adc1278315ed214f0
Leven <1846087763@qq.com>
2021-01-11 16:38:08 +0800

    修改

---
+++ build/ip.js
@@ -0,0 +1,25 @@
+/**
+ * Created by j_bleach on 2019/3/27 0027.
+ */
+window._buildConfig = {
+    // mapIp: "https://gismp.scu.edu.cn", // 川大
+    // mapIp: "https://gl.swun.edu.cn",  // 西南民大
+    // mapIp: "https://gis.parkbobo.com", // 测试地图
+    //mapIp: "http://192.168.4.241/cmgis-server",
+     mapIp: "https://yxceshi.parkbobo.com/cmgis-server/",
+    //mapIp: "https://map.parkbobo.com/gis-server/",
+    // mapIp: "https://cmdown.parkbobo.com/cmgis-server",
+    // mapIp: "https://map.parkbobo.com", // 正式地图
+    // mapIp: "https://map.parkbobo.com/gis-server", // 正式地图
+    // normalIp: "https://gisgd.scu.edu.cn",  // 川大定位
+    // normalIp: "https://gisapp.swun.edu.cn", // 西南民大定位
+    // normalIp: "https://locmap.parkbobo.com", // 测试定位
+    normalIp: "https://yxceshi.parkbobo.com/wsngt-server",
+    // normalIp: "https://xz.parkbobo.com", // 正式定位
+    token: "bG9jYXRpb246NGFiYWE1NzI2ODc0YWIwY2IwMDhkY2RkNzBkYzQ3ZWM=", // token
+    version: "v1", // 版本
+    showMarker: true, // 正式版false(显示定位)
+    log: true, // 正式版false(显示日志)
+    collect: false, // 正式版false(采集数据),
+    navVersion: 'v3.1.1(gps 连续不定位)'
+};
